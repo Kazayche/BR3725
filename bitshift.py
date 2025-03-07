c3a2a7546d8682a796ee572b2862ce65fec8c529
Shourya Pattabhi <96268277+Kazayche@users.noreply.github.com>
2025-03-07 15:07:29 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,9 @@
-def multiply(n):
-    #Add the code and an explanation on how to 
-    #multiply a number by 2 using bitshift
-    return n
+x = int(input("First positive value: "))
+y = int(input("Second positive value: "))
 
+z = (x * y) / 10
+z = int(z)
+bit_shift_value = 1 << z  # shift 1 to the left by z value which is derved from amount of zeros in the product 
+
+# Print the result
+print(bit_shift_value,"Is the value ")
